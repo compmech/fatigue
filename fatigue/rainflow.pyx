ba6c5f1fb24b829b6edc64d896ce96ae40cef10b
saullocastro <saullogiovani@gmail.com>
2017-01-11 22:41:56 -0200

    Initial commit\

--- fatigue/rainflow.pyx
+++ fatigue/rainflow.pyx
@@ -6,10 +6,33 @@ ctypedef np.float64_t CDOUBLE
 cdef extern from "./rainflow.h":
     void rainflowc(int n, double *points, double *array_out)
 
+
+cdef np.ndarray[ndim=1, dtype=CDOUBLE] sig2ext(np.ndarray[ndim=1, dtype=CDOUBLE] sig):
+    cdef int dt=1
+    cdef np.ndarray[ndim=1, dtype=CDOUBLE] w1, ext
+    w1 = np.diff(sig)
+    ext = np.zeros_like(w1)
+    test = (w1[:-1]*w1[1:]) <= 0
+    ext = np.concatenate((np.array([1.]), sig[test], np.array([1.])))
+
+    w1 = np.diff(ext)
+    test = (w1[:-1]==0) & (w1[1:]==0)
+    ext = np.concatenate((np.array([0.]), w1[~test], np.array([0.])))
+
+    test = ext[:-1]==ext[1:]
+    ext = np.concatenate((np.array([0.]), ext[~test], np.array([0.])))
+    if ext.shape[0] > 2:
+        w1 = np.diff(ext)
+        ext = w1[(w1[:-1]*w1[1:]) < 0]
+    return ext
+
+
 def rainflow(np.ndarray[ndim=1, dtype=CDOUBLE] points):
     cdef int n = points.shape[0]
+    cdef np.ndarray[ndim=1, dtype=CDOUBLE] ext
     cdef np.ndarray[ndim=2, dtype=CDOUBLE] array_out
     array_out = np.zeros((n, 3), dtype=np.float64)
-    rainflowc(n, &points[0], &array_out[0, 0])
+    ext = sig2ext(points)
+    rainflowc(n, &ext[0], &array_out[0, 0])
     return array_out
 
--- fatigue/test.py
+++ fatigue/test.py
@@ -2,4 +2,8 @@ import numpy as np
 import pyximport; pyximport.install()
 from rainflow import rainflow
 
-print(rainflow(np.array([0, 1, 2, 1, 2, 0.])))
+a = np.array([0., -1, 1., -1, 1, -1, 1, -1, 0.])
+out = rainflow(a)
+print(out)
+print(np.histogram(out))
+
